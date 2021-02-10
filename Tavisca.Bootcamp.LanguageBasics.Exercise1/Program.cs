8ecbbbe08385f01dd62970c92a34ace07d18b2c5
Akash Singh <asingh1@internal.tavisca.com>
2021-02-10 14:12:18 +0530

    Language-Basic-01 Assignment Completed.

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,57 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int index = 0, calculation = 0;
+            string finddigit = "";
+
+            int[] integerNumbersArray = new int[3];
+
+            string[] stringNumbersArray = equation.Split(new Char[] { '*', '=' });
+
+            foreach (string number in stringNumbersArray)
+            {
+                if (string.IsNullOrWhiteSpace(number) && number.Length > 4)
+                {
+                    return -1;
+                }
+                bool numberConvertSuccessfullyOrNot = int.TryParse(number, out integerNumbersArray[index++]);
+            }
+
+            if (integerNumbersArray[0] == 0)
+            {
+                if (integerNumbersArray[2] % integerNumbersArray[1] != 0) return -1;
+
+                calculation = integerNumbersArray[2] / integerNumbersArray[1];
+                finddigit = stringNumbersArray[0];
+            }
+            else if (integerNumbersArray[1] == 0)
+            {
+                if (integerNumbersArray[2] % integerNumbersArray[0] != 0) return -1;
+
+                calculation = integerNumbersArray[2] / integerNumbersArray[0];
+                finddigit = stringNumbersArray[1];
+            }
+            else
+            {
+                calculation = integerNumbersArray[0] * integerNumbersArray[1];
+                finddigit = stringNumbersArray[2];
+            }
+
+            string matchFindDigitValue = Convert.ToString(calculation);
+
+            if (matchFindDigitValue.Length != finddigit.Length)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < finddigit.Length; i++)
+            {
+                if (finddigit[i] == '?')
+                {
+                    return int.Parse(matchFindDigitValue[i].ToString());
+                }
+            }
+            return 0;
         }
     }
 }
